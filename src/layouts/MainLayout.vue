08f398558c5123ed4606b324ba91adbabc02f08f
EdMendez00 <mm19247@ues.edu.sv>
2025-06-10 08:00:58 -0600

    Agrega menu, y estilo a la barra superior

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,37 +1,27 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+    <q-header elevated class="border-b-4 border-[#F7BC20]">
+      <q-toolbar class="h-23 bg-white text-black">
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <div class="text-3xl ml-5 font-bold">Sistema de Gestión Empresarial</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-[#0250DC]">
       <q-list>
         <q-item-label
           header
+          class="flex h-24 border-b-3 border-[#F7BC20] mb-20 justify-center items-center"
         >
-          Essential Links
+          <q-img
+            class="max-w-[200px] max-h-[60px] object-contain"
+            src="../assets/[CITYPNG.COM]Walmart Horizontal White Logo Image PNG - 3000x3000 1.png"
+          />
         </q-item-label>
 
         <EssentialLink
+          class="text-white font-bold text-lg"
           v-for="link in linksList"
           :key="link.title"
           v-bind="link"
@@ -51,52 +41,30 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: 'Inicio',
+    icon: 'home',
+    link: 'https://quasar.dev',
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: 'Empleados',
+    icon: 'people',
+    link: 'https://github.com/quasarframework',
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: 'Prestaciones',
+    icon: 'calculate',
+    link: 'https://chat.quasar.dev',
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: 'Ausencias',
+    icon: 'calendar_today',
+    link: 'https://forum.quasar.dev',
   },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
 ]
 
 const leftDrawerOpen = ref(false)
 
-function toggleLeftDrawer () {
+function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
 </script>
